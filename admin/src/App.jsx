30a53d850f12e74908ae9f007da43b53e63acf9e
thielang <xuanthuc123412@gmail.com>
2025-07-08 23:13:53 +0700

    fix name import navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";

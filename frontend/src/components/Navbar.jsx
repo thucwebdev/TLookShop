e51d29b3167a1ea46e842d08f970550d7bbc64ea
thielang <xuanthuc123412@gmail.com>
2025-07-08 23:28:04 +0700

    chore/title_hamburger

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -69,10 +69,10 @@ const Navbar = () => {
                 <img className='h-4 rotate-180 ' src={assets.dropdown_icon} alt="" />
                 <p>Back</p>
               </div>
-              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
-              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
-              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/'>TRANG CHỦ</NavLink>
+              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/collection'>SẢN PHẨM</NavLink>
+              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/about'>GIỚI THIỆU</NavLink>
+              <NavLink onClick = {() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>LIÊN HỆ</NavLink>
             </div>
 
         </div>

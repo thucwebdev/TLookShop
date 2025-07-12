995cc365f89a0126b0591492979e82a80d0c8c66
thielang <xuanthuc123412@gmail.com>
2025-07-13 04:18:29 +0700

    update

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import list_order_icon from './list_order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.jpg'
 import delete_icon from './delete_icon.png'
+import user_icon from './user_icon.png'
+import thongke_icon from './icon_thongke.png'
+import dashboard_icon from './dashboard.png'
 
 export const assets = {
     logo,
@@ -13,6 +16,9 @@ export const assets = {
     list_order_icon,
     upload_area,
     parcel_icon,
-    delete_icon
+    delete_icon,
+    user_icon,
+    thongke_icon,
+    dashboard_icon
 }
 
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,19 +7,19 @@ const Sidebar = () => {
     <div className="w-[18%] min-h-screen border-r-2 border-r-gray-300">
       <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
         <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/">
-          <div className="w-5 h-5 bg-gray-500 rounded-full flex items-center justify-center text-white text-xs font-bold">
-            🏠
+          <div className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold">
+             <img src={assets.dashboard_icon} alt="" />
           </div>
           <p className="hidden md:block">Dashboard</p>
         </NavLink>
 
         <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/add">
-          <img className="w-5 h-5 icon" src={assets.add_icon} alt="" />
+          <img className="w-6 h-6 icon" src={assets.add_icon} alt="" />
           <p className="hidden md:block">Thêm sản phẩm</p>
         </NavLink>
 
         <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/list">
-          <img className="w-5 h-5 icon" src={assets.order_icon} alt="" />
+          <img className="w-6 h-6 icon" src={assets.order_icon} alt="" />
           <p className="hidden md:block">Danh sách sản phẩm</p>
         </NavLink>
 
@@ -29,8 +29,8 @@ const Sidebar = () => {
         </NavLink>
 
         <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/users">
-          <div className="w-5 h-5 bg-blue-500 rounded-full flex items-center justify-center text-white text-xs font-bold">
-            U
+          <div className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold">
+            <img src={assets.user_icon} alt="" />
           </div>
           <p className="hidden md:block">Quản lý tài khoản khách hàng</p>
         </NavLink>
@@ -39,8 +39,8 @@ const Sidebar = () => {
           className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l"
           to="/statistics"
         >
-          <div className="w-5 h-5 bg-green-500 rounded-full flex items-center justify-center text-white text-xs font-bold">
-            📊
+          <div className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold">
+            <img src={assets.thongke_icon} alt="" />
           </div>
           <p className="hidden md:block">Thống kê & Báo cáo</p>
         </NavLink>
